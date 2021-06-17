c00d8a633645143d01241c741e925347ad21c85b
Group Task <>
2021-06-17 00:39:49 -0400

    * Group Members:  * 1)Abhilakshay  * 2)John Jester  * 3)Bhawana  * 4)Jyoti

---
+++ src/ca/sheridancollege/project/Player.java
@@ -0,0 +1,53 @@
+/**
+ * Lucky Card Game Project  Base code.
+ * Date:17-06-2021
+ * Group Name:ABJJ Team
+ * Group Members:
+ * 1)Abhilakshay
+ * 2)John
+ * 3)Bhawna
+ * 4)Jyoti
+ */
+package ca.sheridancollege.project;
+
+/**
+ * A class that models each Player in the game. Players have an identifier, which should be unique.
+ * @author dancye, 2018
+ */
+public abstract class Player 
+{
+    private String playerID; //the unique ID for this player
+    
+    /**
+     * A constructor that allows you to set the player's unique ID
+     * @param name the unique ID to assign to this player.
+     */
+    public Player(String name)
+    {
+        playerID= name;
+    }
+    
+    /**
+     * @return the playerID
+     */
+    public String getPlayerID() 
+    {
+        return playerID;
+    }
+
+    /**
+     * Ensure that the playerID is unique
+     * @param givenID the playerID to set
+     */
+    public void setPlayerID(String givenID) 
+    {
+        playerID = givenID;
+    }
+    
+    /**
+     * The method to be instantiated when you subclass the Player class
+     * with your specific type of Player and filled in with logic to play your game.
+     */
+    public abstract void play();
+    
+}
